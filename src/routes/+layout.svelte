1d7d85998f497e83bc582b38987d99643a5d48b2
arcbjorn <arky.happ@gmail.com>
2025-08-03 19:38:03 -0300

    feat: improve favicon rendering with multiple format support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,9 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="icon" type="image/svg+xml" href={favicon} />
+	<link rel="apple-touch-icon" href="/favicon.png" />
 </svelte:head>
 
 {@render children?.()}
